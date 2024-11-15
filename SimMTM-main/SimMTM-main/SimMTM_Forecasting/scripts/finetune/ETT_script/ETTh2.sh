5e6378454fa21f2d518406cca44f0fabfbeab0f4
maochaoren <f22j20f18@163.com>
2024-11-16 01:15:05 +0800

    1116 1

--- SimMTM-main/SimMTM-main/SimMTM_Forecasting/scripts/finetune/ETT_script/ETTh2.sh
+++ SimMTM-main/SimMTM-main/SimMTM_Forecasting/scripts/finetune/ETT_script/ETTh2.sh
@@ -13,14 +13,13 @@ for pred_len in 96 192 336 720; do
         --seq_len 336 \
         --label_len 48 \
         --pred_len $pred_len \
-        --e_layers 2 \
+        --e_layers 1 \
         --enc_in 7 \
         --dec_in 7 \
         --c_out 7 \
-        --n_heads 8 \
-        --d_model 8 \
-        --d_ff 32 \
-        --dropout 0.4 \
-        --head_dropout 0.2 \
+        --n_heads 16 \
+        --d_model 16 \
+        --d_ff 64 \
+        --dropout 0.2 \
         --batch_size 16
 done
